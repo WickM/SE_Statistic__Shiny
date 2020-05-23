db1ef20350b879f691b7b2a1820f7b1880fe4b5e
Manuel <manuel.wick@gmail.com>
2020-05-23 09:52:01 +0200

    fixded git merge errors the app is running again

--- SE_Statistic_Shiy_App/server.R
+++ SE_Statistic_Shiy_App/server.R
@@ -28,14 +28,16 @@ load("data/aufb_covid_data_shiny.RData")
 # ShinyApp ab hier
 shinyServer(function(input, output) {
 
-<<<<<<< HEAD
   # Tab 1 Plot ################################################################################
-  output$text <- renderText(input$text)
-
-  output$t2_map <- renderLeaflet({
-    leaflet() %>%
-      addProviderTiles(providers$CartoDB.Positron)
+  output$t1_text <- renderText(input$t1_text)
+  
+  ## filter the data
+  filtered_data <- reactive({
+    dat_mobil_change %>% 
+      dplyr::filter(Movement_type == "walking" | Movement_type == "driving" | Movement_type == "stay at home") %>% 
+      dplyr::filter(name == input$t1_country, dataset == input$t1_dataset)
   })
+
   plot <- reactive({
     ggplot(filtered_data(), aes(juliandate, val), col = Movement_type) +
       geom_jitter(alpha = 0.6, aes(color = Movement_type)) +
@@ -48,19 +50,9 @@ shinyServer(function(input, output) {
       coord_cartesian(xlim = c(0, 110), ylim = c(-100, 100))
   })
 
-  output$plot1 <- renderPlotly(ggplotly(plot()))
+  output$t1_plot <- renderPlotly(ggplotly(plot()) )
 
-
-  ## filter the data
-  filtered_data <- reactive({
-    dplyr::filter(
-      subset(dat_mobil_change, Movement_type == "walking" | Movement_type == "driving" | Movement_type == "stay at home"),
-      name == input$name, dataset == input$dataset
-    )
-  })
-
-
-  output$table <- renderTable(filtered_data() %>%
+  output$t1_table <- renderTable(filtered_data() %>%
     group_by(Movement_type) %>%
     summarise(
       "Mean" = mean(val),
@@ -70,30 +62,36 @@ shinyServer(function(input, output) {
       "Max" = max(val)
     ))
 
-  # Tab 2 Leaflet ##########
+  #Tab 2 Leaflet ##########    
   # Ab hier Code fuer Tab 2 Plot
+  
+  output$t2_map <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles(providers$CartoDB.Positron)
+  })
+  
   observeEvent(c(input$t2_mapType, input$t2_date), {
-    leafletProxy("t2_map") %>%
+    leafletProxy("t2_map") %>% 
       clearMarkers() %>%
       clearShapes() %>%
-      addCircles(
-        data = t2_data(input$t2_mapType),
-        lat = ~latitude,
-        lng = ~longitude,
-        label = ~ paste(name, ":", val),
-        weight = 1,
-        radius = ~ log(abs(val), 2) * 25000,
-        fillOpacity = 0.1,
-        color = ~ ifelse(val > 0, "red", "green")
-      )
+      addCircles(data = t2_data(input$t2_mapType),
+                 lat = ~latitude, 
+                 lng = ~longitude , 
+                 label = ~paste(name, ":", val),
+                 weight = 1,
+                 radius = ~log(abs(val),2)*25000,
+                 fillOpacity = 0.1, 
+                 color = ~ifelse(val>0,"red","green"))
   })
-
-  t2_data <- function(t2_type) {
+  
+  t2_data <- function(t2_type){
     t2_data_help <- data.frame(dat_mobil_change_tab_plot)
-    t2_data_help <- t2_data_help[t2_data_help$percent_change_type == input$t2_mapType, ]
+    t2_data_help <- t2_data_help[t2_data_help$percent_change_type == input$t2_mapType, ] 
     return(t2_data_help[t2_data_help$date == input$t2_date, ])
+    
   }
-
+  
+  
   # Tab 3 Markdown #######################################################
   # Ab hier Code fuer Tab 3 Plot
   # Markdown Teil der Shiny App
@@ -147,124 +145,7 @@ shinyServer(function(input, output) {
     return(dat_mobil_change_filter)
   })
 
-
-
   table_filter_slow <- shiny::throttle(r = table_filter_small, millis = 1000)
-
-  output$tb3_table <- shiny::renderTable(table_filter_slow())
-=======
-#Tab 1 Plot ################################################################################    
-    output$t1_text <- renderText(input$t1_text)
-    
-    output$t1_plot <- renderPlot({
-        # Plot filtered data
-
-        ggplot(filtered_data(), aes(juliandate,val),col = Movement_type) +
-            geom_jitter(alpha = 0.6, aes(color = Movement_type)) +
-            geom_smooth(method = loess, fullrange = TRUE, aes(color = Movement_type)) +
-            geom_hline(yintercept = 0, linetype="dashed", 
-                       color = "grey20", size=0.7)+
-            labs( x= "Julian date", y ="Change of Movement in %", size = 10)+
-            coord_cartesian(xlim = c(0, 110), ylim = c(-100,100))
-
-    })
-
-    ## filter the data
-    filtered_data <- reactive({
-        dplyr::filter(subset(dat_mobil_change, Movement_type =="walking" | Movement_type =="driving"| Movement_type =="stay at home"),
-                      name == input$t1_country, dataset ==input$t1_dataset)
-    })
-
-    output$t1_table <- renderTable(filtered_data() %>%
-                                    group_by(Movement_type) %>%
-                                    summarise("Mean" = mean(val), 
-                                              "Median" = median(val),
-                                              "STDEV" = sd(val), 
-                                              "Min" = min(val),
-                                              "Max" = max(val)))
-    
-    #Tab 2 Leaflet ##########    
-    # Ab hier Code fuer Tab 2 Plot
-    
-    output$t2_map <- renderLeaflet({
-      leaflet() %>%
-        addProviderTiles(providers$CartoDB.Positron)
-    })
-    
-    observeEvent(c(input$t2_mapType, input$t2_date), {
-        leafletProxy("t2_map") %>% 
-        clearMarkers() %>%
-        clearShapes() %>%
-        addCircles(data = t2_data(input$t2_mapType),
-                         lat = ~latitude, 
-                         lng = ~longitude , 
-                         label = ~paste(name, ":", val),
-                         weight = 1,
-                         radius = ~log(abs(val),2)*25000,
-                         fillOpacity = 0.1, 
-                         color = ~ifelse(val>0,"red","green"))
-    })
-
-     t2_data <- function(t2_type){
-         t2_data_help <- data.frame(dat_mobil_change_tab_plot)
-         t2_data_help <- t2_data_help[t2_data_help$percent_change_type == input$t2_mapType, ] 
-         return(t2_data_help[t2_data_help$date == input$t2_date, ])
-             
-     }
-
-    #Tab 3 Markdown ####################################################### 
-    # Ab hier Code fuer Tab 3 Plot
-    # Markdown Teil der Shiny App 
-    observeEvent(input$generate_Report, {
-        shiny::withProgress(message = 'Report in progress',
-                            detail = 'This may take a while',
-                            {
-                                data <- table_filter_big()
-                                shiny::setProgress(value = 0.3, message = "knit Report")
-                                rmarkdown::render('Bericht/Bericht.Rmd',
-                                output_format = "html_document",
-                                    params = list(
-                                    data = data
-                                    )
-                                )
-                            shiny::setProgress(value = 0.9, message = "Reporting done",detail="")
-                            shiny::appendTab(inputId = "nav_bar", tabPanel(title = "Report",
-                                                    fluidPage(
-                                                        fluidRow(
-                                                            column(12,
-                                                                   shiny::includeHTML("Bericht/Bericht.html")
-                                                                ))))
-                            )
-                            }
-                    )
-        })
-    
-    table_filter_big <- reactive({
-        dat_mobil_change_filter <- dat_mobil_change_tab_markdown %>% 
-            dplyr::filter(name %in% input$t3_countries & 
-                              percent_change_type %in% input$t3_activity & 
-                              date > input$t3_date[1] & 
-                              date < input$t3_date[2]) %>% 
-            dplyr::arrange(name, date)
-        return(dat_mobil_change_filter)
-    })
-    
-    table_filter_small <- reactive({
-        dat_mobil_change_filter <- table_filter_big()
-        
-        dat_mobil_change_filter <- dat_mobil_change_filter %>% 
-            dplyr::mutate(month = lubridate::month(date,label =TRUE)) %>% 
-            group_by(name,month,percent_change_type) %>% 
-            summarise(p_change = mean(val, na.rm = TRUE))
-        
-        return(dat_mobil_change_filter)
-    })
-    
-    
-
-    table_filter_slow <- shiny::throttle(r = table_filter_small, millis = 1000)
-   
-    output$tb3_table <- shiny::renderTable(table_filter_slow() )
->>>>>>> 9d05bf718f6815431c15c0faf8ca0107796d2078
+  output$tb3_table <- shiny::renderTable(expr = table_filter_slow() )
+  
 })
-
--- SE_Statistic_Shiy_App/ui.R
+++ SE_Statistic_Shiy_App/ui.R
@@ -16,42 +16,37 @@ load("data/aufb_covid_data_shiny.RData")
 
 CoronaVirusImageLink <- "https://upload.wikimedia.org/wikipedia/commons/8/82/SARS-CoV-2_without_background.png"
 
-<<<<<<< HEAD
 # Define UI for application that draws a histogram
 shinyUI(
   navbarPage("SE Statistic Shiny APP",
     id = "nav_bar",
-    # Tab1----#####################################################################
-    tabPanel(
-      "Plot",
-      sidebarLayout(
-        sidebarPanel(
-          # Ab hier Code fuer Sidebar
-          selectInput("name", "Please select Country",
-            choices = c("Austria", "Sweden", "Germany", "Italy", "Japan")
-          ),
-          checkboxGroupInput("dataset", "Source", choices = c("Apple", "Google"), selected = c("Apple", "Google")),
-          textInput("text", "Enter some text to be displayed", "")
-        ),
-
-        mainPanel(
-          h3(textOutput("text"), width = 6, align = "center"),
-          tableOutput("table"),
-          width = 6, align = "Center",
-          fluidPage(
-            fluidRow(
-              column(
-                width = 12, align = "Center",
-                plotlyOutput("plot1", height = 350),
-              ),
-              # actionButton("exclude_toggle", "Toggle points"),
-              # actionButton("exclude_reset", "Reset")
-            )
-          )
-        )
-      )
+    # Plot Tab ----#####################################################################
+    tabPanel("Plot",
+             sidebarLayout(
+               sidebarPanel(
+                 selectInput("t1_country", 
+                             label = "Please select Country",
+                             choices = c("Austria", "Sweden", "Germany", "Italy", "Japan")),
+                 checkboxGroupInput("t1_dataset",
+                                    label = "Source", 
+                                    choices = c("Apple","Google"), 
+                                    selected = c("Apple","Google")),
+                 textInput("t1_text", 
+                           label = "Enter Title", 
+                           value = "Title"),
+                 br(),
+                 
+                 img(src=CoronaVirusImageLink, width = "100%")
+               ),
+               
+               mainPanel(
+                 h2(textOutput("t1_text")),
+                 tableOutput("t1_table"),
+                 plotlyOutput("t1_plot")
+               )
+             )
     ),
-
+    
     # Tab2----#####################################################################
 
     tabPanel(
@@ -92,72 +87,6 @@ shinyUI(
       )
     ),
 
-=======
-shinyUI(navbarPage("SE Statistic Shiny APP", id = "nav_bar",
-                   
-# Plot Tab ----#####################################################################
-tabPanel("Plot",
-         sidebarLayout(
-           sidebarPanel(
-             selectInput("t1_country", 
-                         label = "Please select Country",
-                         choices = c("Austria", "Sweden", "Germany", "Italy", "Japan")),
-             checkboxGroupInput("t1_dataset",
-                                label = "Source", 
-                                choices = c("Apple","Google"), 
-                                selected = c("Apple","Google")),
-             textInput("t1_text", 
-                       label = "Enter Title", 
-                       value = "Title"),
-             br(),
-
-             img(src=CoronaVirusImageLink, width = "100%")
-            ),
-           
-           mainPanel(
-             h2(textOutput("t1_text")),
-             tableOutput("t1_table"),
-             plotOutput("t1_plot")
-           )
-         )
-),
-
-# Leaflet Tab ----#####################################################################
-tabPanel("Leaflet",
-         sidebarLayout(
-           
-           sidebarPanel(
-             
-             h5("COVID-19 induced reduction in population mobility"),
-             
-             selectInput("t2_mapType",
-                         label = "mobility metric",
-                         choices = c(
-                           "driving"="driving_percent_change_from_baseline",
-                           "walking"="walking_percent_change_from_baseline",
-                           "transit"="transit_percent_change_from_baseline",
-                           "retail store visits"="retail_and_recreation_percent_change_from_baseline",
-                           "grocery store visits"="grocery_and_pharmacy_percent_change_from_baseline",
-                           "park visits"="parks_percent_change_from_baseline",
-                           "train station usage"="transit_stations_percent_change_from_baseline",
-                           "workplaces occupation"="workplaces_percent_change_from_baseline",    
-                           "staying at home"="residential_percent_change_from_baseline"),
-                         selected = "driving_percent_change_from_baseline"),
-             
-             sliderInput("t2_date",
-                         label = "Date input",
-                         min = as.Date(min(dat_mobil_change_tab_plot$date)),
-                         max = as.Date(max(dat_mobil_change_tab_plot$date)),  
-                         value = as.Date(min(dat_mobil_change_tab_plot$date)),
-                         timeFormat = "%d %b",
-                         animate = animationOptions(interval = 1000, loop = FALSE)
-             ),
-           ),
-           mainPanel(leafletOutput("t2_map"))
-         )
-),
->>>>>>> 9d05bf718f6815431c15c0faf8ca0107796d2078
-
     # Tab3----#####################################################################
     tabPanel(
       "Markdown",
@@ -176,7 +105,6 @@ tabPanel("Leaflet",
           actionButton(inputId = "generate_Report", label = "generate Report")
         ),
 
-<<<<<<< HEAD
         mainPanel(
           shiny::tableOutput(outputId = "tb3_table")
         )
@@ -184,26 +112,3 @@ tabPanel("Leaflet",
     )
   ) # Navbar Ende´
 ) # shinUi Ende
-=======
-# Markdown Tab ----#####################################################################
-  tabPanel("Markdown",
-    sidebarLayout(
-      sidebarPanel(width = 4,
-        shiny::selectizeInput(inputId = "t3_countries", label="Countries", multiple =TRUE, choices = unique(dat_mobil_change_tab_markdown$name)),
-        shiny::selectizeInput(inputId = "t3_activity", label="Activity Type", multiple =TRUE, choices = unique(dat_mobil_change_tab_markdown$percent_change_type), selected=  unique(dat_mobil_change_tab_markdown$percent_change_type)),
-        shiny::sliderInput(inputId = "t3_date", label= "Date",
-                           min = min(dat_mobil_change_tab_markdown$date), 
-                           max = max(dat_mobil_change_tab_markdown$date),
-                           value = c(min(dat_mobil_change_tab_markdown$date) ,max(dat_mobil_change_tab_markdown$date))),
-        shiny::tags$br(), shiny::tags$hr(),
-        actionButton(inputId = "generate_Report", label = "generate Report")
-        ), 
-                                 
-      mainPanel(
-        shiny::tableOutput(outputId = "tb3_table")
-        )
-      )
-    )
-)
-)
->>>>>>> 9d05bf718f6815431c15c0faf8ca0107796d2078
